c4261447ae8dd77f38bf93f0300d0fed3ffc0312
MartaMykhailyna <marta.mykhailyna-pi2002@kep.nung.edu.ua>
2023-05-21 21:08:47 +0300

    dfs

--- ind_zavd/js/our-services-carousel1.js
+++ ind_zavd/js/our-services-carousel1.js
@@ -1,7 +1,19 @@
 const carousel = document.querySelector('.our-services__carousel');
 const slides = carousel.querySelectorAll('.our-services__slide');
-const slideWidth = slides[0].offsetWidth;
 let currentPosition = 0;
+
+function updateSlidePosition() {
+  const windowWidth = window.innerWidth;
+  let slideWidth;
+
+  if (windowWidth >= 1000) {
+    slideWidth = carousel.offsetWidth / 3;
+  } else {
+    slideWidth = carousel.offsetWidth;
+  }
+
+  carousel.style.transform = `translateX(-${slideWidth * currentPosition}px)`;
+}
 
 function moveToNextSlide() {
   if (currentPosition === slides.length - 3) {
@@ -12,8 +24,26 @@ function moveToNextSlide() {
   updateSlidePosition();
 }
 
-function updateSlidePosition() {
-  carousel.style.transform = `translateX(-${ slideWidth*currentPosition }px)`;
-}
+// setInterval(moveToNextSlide, 3000);
 
-setInterval(moveToNextSlide, 3000);
+const buttonPrev = document.querySelector(".our-services__slideshow-btn-left");
+buttonPrev.addEventListener("click", () => {
+  if (currentPosition === 0) {
+    currentPosition = slides.length - 3;
+  } else {
+    currentPosition--;
+  }
+  updateSlidePosition();
+});
+
+const buttonNext = document.querySelector(".our-services__slideshow-btn-right");
+buttonNext.addEventListener("click", () => {
+  if (currentPosition === slides.length - 3) {
+    currentPosition = 0;
+  } else {
+    currentPosition++;
+  }
+  updateSlidePosition();
+});
+
+window.addEventListener("resize", updateSlidePosition);
